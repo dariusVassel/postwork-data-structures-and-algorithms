8d3c009ceebd298ba6cf250f2cfa8e1c109bb8e6
samarth-gwalani <108356851+samarth-gwalani@users.noreply.github.com>
2022-10-25 16:36:06 -0400

    Solution 3

--- 01-week-1--starter-algorithms/02-day-3--fibonacci-series/javascript/fibonacci.js
+++ 01-week-1--starter-algorithms/02-day-3--fibonacci-series/javascript/fibonacci.js
@@ -1,5 +1,20 @@
 function fibonacci(num) {
   // type your code here
+  let fibonacciSeries = []
+
+  for (let i=0; i <= num; i++){
+    if(i == 0){
+      fibonacciSeries.push(i)
+    }
+    else if(i == 1){
+      fibonacciSeries.push(i)
+    }
+    else{
+      fibonacciSeries.push(fibonacciSeries[i-1] + fibonacciSeries[i-2])
+    }  
+  }
+  console.log(fibonacciSeries)
+  return(fibonacciSeries[num])
 }
 
 if (require.main === module) {
@@ -16,6 +31,9 @@ if (require.main === module) {
 
   console.log("Expecting: 55");
   console.log("=>", fibonacci(10));
+
+  console.log("Expecting: 55");
+  console.log("=>", fibonacci(20));
 }
 
 module.exports = fibonacci;
